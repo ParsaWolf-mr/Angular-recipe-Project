86a9d2eb7d875a4dc9f3985b59a4789db33e194c
Mojtaba Rostami <rostamimojtaba15@gmail.com>
2021-10-29 18:29:39 +0200

    creating Angular

---
+++ src/main/java/com/example/angularrecipeproject/AngularRecipeProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.angularrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularRecipeProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AngularRecipeProjectApplication.class, args);
+    }
+
+}
